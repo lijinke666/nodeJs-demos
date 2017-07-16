e43d466240491047ee59b63b5c8785d6e1d60e22
jinke.li <1359518268@qq.com>
2017-07-16 20:42:23 +0800

    feat:增加单元测试

---
+++ mocha.js
@@ -0,0 +1,17 @@
+// 单元测试练习
+// 这里没有用chai 断言库 不好用
+
+const assert = require('assert')
+
+describe('Array',()=>{
+    let arr
+    beforeEach(()=>{
+        arr = [3,2,1,10]
+    })
+    describe('#sort()',()=>{
+        it('需要按重小到大排序',()=>{
+            const test = [1,2,3,10]
+            assert(arr.sort() === test)
+        })
+    })
+})
