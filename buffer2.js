4f07b18b104aa65ef7fa8a65e69d5a4e95e64efe
jinke <1359518268@qq.com>
2017-09-15 20:49:26 +0800

    feat:add

--- buffer2.js
+++ buffer2.js
@@ -5,19 +5,12 @@ const Int64 = require('node-int64')
 const decoder = new StringDecoder('utf8')
 const os = require('os')
 
-const x = new Int64(0x123456789)
-const y = new Int64('123456789abcdef0')
-// const buf = Buffer.allocUnsafe(51)
-
 const createBuffer = (size) => (fnName) => (content, offset = 0) => {
     const BYTE = "BE"
     const _Buffer = Buffer.allocUnsafe(size).fill(0)
     _Buffer[fnName](content, offset)
     return _Buffer
 }
-// buf.writeInt16BE(0,2)
-// buf.writeInt32BE(0,4)
-// consoRE.log(decoder.write(buf));
 
 const create64Buffer = (size = 8) => (value) => {
     const nullBuffer = Buffer.alloc(8)
@@ -26,11 +19,19 @@ const create64Buffer = (size = 8) => (value) => {
     buffer_64.copy(nullBuffer, 0)
     return nullBuffer
 }
-// var buf4 = new Buffer(8) // 8字节的空buffer
-// var x = new Int64(1056) // 64进制的1057
-// var buffer_64 = x.toBuffer() // 取到64进制的1057的buffer
-// buffer_64.copy(buf4, 0) // 将64进制的值赋值给buf4
 
+function createHeader() {
+    const totalLength = 59       // 4 + 51 + 4
+    const _Buffer = Buffer.alloc(totalLength).fill(0)
+    let offset = 0
+    offset += _Buffer.writeInt32BE(totalLength, offset)
+    offset += _Buffer.writeInt16BE(0, offset)
+    offset += _Buffer.writeInt32BE(0, offset)
+    offset += _Buffer.writeInt32BE(0, offset)
+    const uidBuffer = create64Buffer(8)(0x11223344)
+    const newBuffer = Buffer.concat([_Buffer, uidBuffer], _Buffer.length + uidBuffer.length)
+}
+// createHeader()
 
 /**
  * 消息头部结构
@@ -45,22 +46,23 @@ const create64Buffer = (size = 8) => (value) => {
  * @return HEADERS
  */
 
-function buildHeaders({ uid, msgId = 30000 } = options) {
+function buildHeaders({ uid, msgId = 30000,msgLength } = options) {
     const version = createBuffer(2)("writeInt16BE")(0)          //版本号
     const sessionId = createBuffer(4)('writeInt32BE')(0)        //会话id
     const tagId = createBuffer(4)('writeInt32BE')(0)            //包序号
     const _uid = create64Buffer(8)(uid)                      //uid             
-    const bodyFlag = createBuffer(1)("writeIntBE")(0)           //bodyFlag
+    const bodyFlag = createBuffer(1)("writeInt8")(0)           //bodyFlag
     const optionLength = createBuffer(4)("writeInt32BE")(0)     //options length
-    const timestamp = create64Buffer(8)(0x11223344)             //时间戳
+    const timestamp = create64Buffer(8)(0)             //时间戳
     const magicNum = createBuffer(2)("writeInt16BE")(0)               //magic num
     const _msgId = createBuffer(2)("writeInt16BE")(msgId)               //消息id
     const msgType = createBuffer(2)("writeInt16BE")(0)                 //消息类型
     const msgSeq = createBuffer(4)("writeInt32BE")(0)                   //消息序列
-    const srcFE = createBuffer(1)("writeIntBE")(0)
-    const destFE = createBuffer(1)("writeIntBE")(0)
+    const srcFE = createBuffer(1)("writeInt8")(0)
+    const destFE = createBuffer(1)("writeInt8")(1)
     const srcId = createBuffer(4)("writeInt32BE")(0)
     const destId = createBuffer(4)("writeInt32BE")(0)
+    const message = Buffer.alloc(4).fill(0)
 
     const config = [
         { value: version, length: version.length },
@@ -80,17 +82,22 @@ function buildHeaders({ uid, msgId = 30000 } = options) {
         { value: destId, length: destId.length }
     ]
 
-    const totalLength = config.reduce((prev, { length }) => prev += length, 0)
+    const totalLength = config.reduce((prev, { length }) => prev += length, 0) + 4 + 4 +msgLength
+    console.log('total',totalLength);
+    const placeHolder = createBuffer(4)("writeInt32BE")(totalLength)
+    const msg = createBuffer(4)("writeInt32BE")(msgLength)
     const _buffers_ = config.map(({ value }) => value)
+    _buffers_.unshift(placeHolder)
+    _buffers_.push(msg)
 
-    const headers = Buffer.concat(_buffers_, totalLength)
-
-    return headers
+    return Buffer.concat(_buffers_, totalLength - msgLength )
 }
 
-const HEADERS = buildHeaders({uid:0x11223344,msgId:30000})
-
-console.log(HEADERS);
+module.exports = {
+    createBuffer,
+    create64Buffer,
+    buildHeaders
+}
 
 
 
--- net.js
+++ net.js
@@ -1,35 +1,73 @@
 const net = require('net')
+const {promisify} = require("util")
+const {PBSSMsg, GMRequestPlayerInfo} = require("./content")
+
+const {
+    buildHeaders,
+    createBuffer
+} = require('./buffer2.js')
+
+
+
+const msgBuf = PBSSMsg.encode({ gmRequestPlayerInfo: {uid:222}}).finish();
+// const msgBuf = GMRequestPlayerInfo.decode(GMRequestPlayerInfo.encode(testMsg).finish())
+console.log('msg',msgBuf,msgBuf.length);
+
+const headers = buildHeaders({uid:222,msgLength:msgBuf.length})
+console.log('headers',headers.length);
+const allBuffer = Buffer.concat([headers,msgBuf],headers.length + msgBuf.length)
+console.log(allBuffer.length);
+
 
 /**
  * 与 c++ 服务器 进行 tcp协议 通信
  */
 const HOST = '192.168.2.154'
 const PORT = 9002
+// console.log(HEADERS,HEADERS.length);
 
-
-var x = 0;
 // const client = new net.Socket()
-const client = net.createConnection(PORT,HOST)
+// const client = net.createConnection(PORT,HOST)
 
-client.on("connect",()=>{
-    console.log('连接成功');
-    var g = new Buffer(6);
-    g.writeInt32BE(2, 0);
-    g.writeInt16BE(2, 4);
-    console.log(g);
-    client.write(g);
+const client = new net.Socket()
+
+client.connect(PORT,HOST,()=>{
+    client.write(allBuffer)
+    console.log('连接成功!');
 })
 
 client.on('data',(data)=>{
-    console.log('data',data);
+    console.log( Buffer.isBuffer(data)); 
+    var buf_len = data.readInt32BE(55);
+    console.log('data===', buf_len);
+    const buf2 = data.slice(55);
+    var obj = PBSSMsg.decode(buf2, buf_len)
+    obj = obj.gmResponsePlayerInfo
+    console.log(obj);
+    console.log(obj.info.uid);
+    console.log(obj.info.diamond);
+    // client.destroy()
+})
+
+client.on('error',(err)=>{
+    console.log('error',err);
 })
 
 client.on('close',()=>{
-    console.log('连接关闭');
+    console.log("close");
 })
 
-
-setTimeout(()=>{
-    console.log('ghjgjkhkljhlhlkh');
-    client.end('')
-},8000)
+// 00 00 00 3e 
+// 00 00
+//  00 00 00 00 
+//  00 00 00 00 
+//  00 00 00 00 00 00 00 de
+//   00
+//    00 00 00 00
+//     00 00 00 00 00 00 00 00
+//      00 00
+//       75 30
+//        00 00 
+//        00 00 00 00 
+//        00 
+//        00 00 00 00
