406b0c768d03410450e36561164b6cd2c9b29da9
jinke <1359518268@qq.com>
2017-10-18 16:18:21 +0800

    feat:增加友盟推送 nodejs 算法

---
+++ umeng.sign.js
@@ -0,0 +1,15 @@
+    //获取友盟 推送签名
+    getUmengSign({
+        method = METHOD,
+        url = SEND_NOTICES_API,
+        secret = APP_MASTER_SECRET,
+        requestParams = {}
+    }) {
+        const postBody = JSON.stringify(requestParams)
+        debug('[Fn => getUmentSign ] postBody => ', postBody)
+        const sign = crypto.createHash('md5')
+            .update(method + url + postBody + secret, 'utf8')
+            .digest('hex')
+        debug('[Fn => getUmentSign ] 签名 => ', sign)
+        return sign
+    },
