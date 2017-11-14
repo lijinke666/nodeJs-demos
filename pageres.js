f8ed6d9f359d56c058273383cbdfbf67d0d958a9
jinke <1359518268@qq.com>
2017-11-14 11:09:06 +0800

    feat:add 网页截图

--- pageres.js
+++ pageres.js
@@ -1,8 +1,9 @@
 //网页截图
+const targetUrl = "http://www.lijinke.cn"
 const path = require("path")
 const Pageres = require("pageres")
 const page = new Pageres({delay:2})
-                .src('zhihu.com',['1024x768','480x320','iphone 5s'],{crop:true})
+                .src(targetUrl,['1024x768','480x320','iphone 5s'],{crop:true})
                 .dest(path.resolve(__dirname,'images'))
                 .run()
                 .then(()=> console.log('网页截图完成'))
