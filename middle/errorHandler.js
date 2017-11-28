b83033a2fce5a151cdfc7b601795fe00aba26943
jinke <1359518268@qq.com>
2017-11-28 15:56:15 +0800

    feat:add errorHandler middleware

---
+++ middle/errorHandler.js
@@ -0,0 +1,24 @@
+const { Error_logger } = require('../../libs/logHelper')
+
+module.exports = function (err, req, res, next) {
+    const resultData = {
+        api: req.originalUrl,
+        method: req.method,
+        message: "ERROR",
+        result: {
+            code: 500,
+            message: err && err.message ? err.message : err
+        }
+    }
+    Error_logger.info('errorHandler:', JSON.stringify({
+        query: req.query,
+        body: req.body,
+        filename: __filename,
+        dirname: __dirname,
+        cwd: process.cwd(),
+        functionName: require.main.filename,
+        resultData
+    }))
+
+    res.status(err.status || 500).send(resultData)
+}
