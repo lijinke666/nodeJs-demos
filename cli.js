4b31471584cb8d0843965c0e71374acfb279c8aa
jinke <1359518268@qq.com>
2018-03-23 12:01:10 +0800

    feat:add

--- cli.js
+++ cli.js
@@ -4,36 +4,51 @@
 //原生逐行读取模块
 const readline = require('readline')
 const chalk = require('chalk')       //彩色输出
-const {exec} = require('child_process')  //子进程
+const { execSync } = require('child_process')  //子进程
+const fs = require('fs')
+const path = require('path')
 const question = '项目名称 : '
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 
 //问问题
-const sayQuestion = (question)=>{
-    return new Promise((res,rej)=>{
+const sayQuestion = (question) => {
+    return new Promise((res, rej) => {
         rl.question(question, (answer) => {
             res(answer)
             rl.close();
-          });
+        });
     })
 }
 
-const init = async ()=>{
-    const name = await sayQuestion(question)
-    console.log(chalk.blue('project name:',name));
-    console.log(chalk.green('loading....'));
-    exec(`mkdir ${name} | cd ${name}`,(error, stdout, stderr)=>{
-        if(error){
-            console.log(chalk.error('create error:',error))
-            process.exit(0)
-        }
-        
-    })
+const createFiles = (name) => {
+    execSync(`mkdir ${name} | cd ${name}`)
+    const _pkg = JSON.stringify({
+        version:"0.0.1",
+        name,
+        author:"Jinke.Li"
+    },undefined,2)
+    fs.writeFileSync(path.resolve(__dirname,name,"package.json"), _pkg)
+    console.log(chalk.green('√ create success!'));
+    process.exit(0)
+}
+
+const init = async () => {
+    try {
+        const name = await sayQuestion(question)
+        console.log(chalk.blue('project name:', name));
+        console.log(chalk.green('loading....'));
+        createFiles(name)
+    } catch (e) {
+        console.log(chalk.red('create faild:',e.message));
+        process.exit(0)
+    }
+
+
 }
 
 init()
