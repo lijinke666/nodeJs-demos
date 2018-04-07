aab8d8c6c95fcbadc0d52b7773bcdd8fdecbf3fc
jinke.li <1359518268@qq.com>
2018-04-07 23:05:32 +0800

    feat:add dns resolve

--- dns.js
+++ dns.js
@@ -1,7 +1,15 @@
 //域名服务器
 const dns = require('dns')
 
-dns.lookup('www.lijinke.cn',(err,address,family)=>{
+const url = "www.lijinke.cn"
+
+//ip地址
+dns.lookup(url,(err,address,family)=>{
     if(err) throw new Error(err)
     console.log(err,address,family)
 })
+
+//解析域名
+dns.lookupService("47.94.21.132", 22, (err, hostname, service) => {
+  console.log(hostname, service);
+});
