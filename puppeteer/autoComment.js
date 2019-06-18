88bd53531d7447d4fea9ff07036e02709a71b49f
Jinke.Li <1359518268@qq.com>
2019-06-18 14:52:16 +0800

    feat(puppeteer): auto comment

--- puppeteer/autoComment.js
+++ puppeteer/autoComment.js
@@ -1,8 +1,7 @@
 /**
- * @name 自动搜索
+ * @name 自动评论
  */
 const puppeteer = require("puppeteer");
-const path = require("path");
 const URL = "http://www.baidu.com";
 
 (async () => {
@@ -16,18 +15,6 @@ const URL = "http://www.baidu.com";
       const page = await browser.newPage();
       await page.goto(URL, { waitUntil: "networkidle2" }); // 等待网络状态为空闲的时候才继续执行
 
-      //获取焦点
-      await page.focus("#kw");         //#kw 百度input 输入框的ID
-      //输入关键字
-      await page.type("#kw",'照片', {
-        delay: 500 // 控制 keypress 也就是每个字母输入的间隔
-      });
-      //触发回车
-      await page.tap("#su");
-
-      await page.waitFor(2000);
-
-      await browser.close();
     } catch (error) {
       console.log("error\n", error);
       process.exit(0);
