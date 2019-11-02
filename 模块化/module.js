bdba1575a73c0e5a77b619aac2203a8382f5f9a3
Jinke.Li <1359518268@qq.com>
2019-11-02 14:48:58 +0800

    feat: module.exports 和 exports 不能共存

--- 模块化/module.js
+++ 模块化/module.js
@@ -3,3 +3,10 @@ module.exports = {
     console.log('test...')
   }
 }
+
+// 虽然 exports 是 module.exports 的引用 但是不能共存
+// 外面调用这个方法会报错
+
+exports.a = () => {
+  console.log('a')
+}
--- 模块化/require.js
+++ 模块化/require.js
@@ -8,4 +8,5 @@ const app = require('./module')
 const app2 = require('./exports')
 
 app.test()
+app.a() // TypeError: app.a is not a function
 app2.test()
